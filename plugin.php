82eacb77d0c802f57510e380361a1ffea23829f6
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-09-28 16:43:35 +0100

    Refactor. Only need the code once for each scripts and styles.

--- plugin.php
+++ plugin.php
@@ -24,114 +24,98 @@ class MPH_Minify {
 	// Useful stuff.
 	var $plugin_url;
 	var $minify_url;
+	var $cache_url;
 
-	var $scripts_queue;
-	var $styles_queue;
+	// Store all assets (handles, and versions), in correct order of processing.
+	var $asset_queue = array();
 
 	function __construct() {
 
 		global $wp_scripts, $wp_styles;
+
 		$this->wp_scripts = $wp_scripts;
 		$this->wp_styles = $wp_styles;
 
 		$this->plugin_url = plugins_url( basename( __DIR__ ) );
 		$this->minify_url = trailingslashit( $this->plugin_url ) . 'php-minify/min/';
+		
+		$this->cache_url = trailingslashit( trailingslashit( WP_CONTENT_URL ) . 'mph_minify_cache' );
 
 	}
 
 	function minify() {
 
-		$this->minify_scripts();
-		$this->minify_styles();
+		$classes = array( 'wp_scripts', 'wp_styles' );
+
+		// Set up the queue of assets.
+		foreach( $classes as $class )
+			$this->get_asset_queue( $class );
+
+		// Process the queue.
+		foreach( $classes as $class )
+			foreach ( $this->asset_queue[ $class ] as $group => $assets  )
+				$this->process_assets( $class, $group );	
 
 	}
 
-	function minify_scripts() {
+	function get_asset_queue( $class ) {
 
 		// Do this to set up the todos - in the correct order.	
-		$this->wp_scripts->all_deps( $this->wp_scripts->queue );
+		$this->$class->all_deps( $this->$class->queue );
 		
-		foreach ( $this->wp_scripts->to_do as $key => $handle )
-			$this->scripts_queue[ $this->wp_scripts->groups[$handle] ][] = $handle;
+		foreach ( $this->$class->to_do as $key => $handle ) {
 
-		foreach ( $this->scripts_queue as $group => $handle )
-			$this->process_scripts( $group );
+			$this->asset_queue[$class][ $this->$class->groups[$handle] ][] = array( 
+				'handle' => $handle,
+				'version' => $this->$class->registered[$handle]->ver
+			);
+
+		}
+
+		return $this->asset_queue;
 
 	}
 
-	function minify_styles() {
+	function process_assets( $class, $group ) {
 
-		// Do this to set up the todos - in the correct order.	
-		$this->wp_styles->all_deps( $this->wp_styles->queue );
-		
-		foreach ( $this->wp_styles->to_do as $key => $handle )
-			$this->styles_queue[ $this->wp_styles->groups[$handle] ][] = $handle;
-		
-		foreach ( $this->styles_queue as $group => $handle )
-			$this->process_styles( $group );
-			
-	}
+		$file = trailingslashit( $this->cache_url ) . crc32( serialize( $this->asset_queue[$class][$group] ) );
 
-	function process_scripts( $group ) {
-		
-		$scripts = array();
+		if ( ! file_exists( $file ) ) {
 
-		foreach ( $this->scripts_queue[$group] as $handle ) {
-			
-			$scripts[] = $this->get_script_path( $handle );
+			$srcs = array();
+			foreach ( $this->asset_queue[$class][$group] as $asset ) {
+				
+				$srcs[] = $this->get_asset_path( $class, $asset['handle'] );
 
-			$this->wp_scripts->dequeue( $handle );
+				$this->$class->dequeue( $asset['handle'] );
+				$this->$class->remove( $asset['handle'] );
 
-		}	
+			}	
 
-		$min_src = $this->minify_url . '/?f=' . implode( ',', $scripts );
-		
-		// Enqueue the scripts
-		$this->wp_scripts->add( 'mph-minify-script-group-' . $group, $min_src );
-		$this->wp_scripts->add_data( 'mph-minify-script-group-' . $group, 'group', $group );		
-		$this->wp_scripts->enqueue( 'mph-minify-script-group-' . $group );
+			$file = $this->minify_url . '/?f=' . implode( ',', array_filter( $srcs ) );
+
+		}
+
+		// Enqueue the assets
+		$this->$class->add( 'mph-minify-' . $class . '-' . $group, $file );
+		$this->$class->add_data( 'mph-minify-' . $class . '-' . $group, 'group', $file );		
+		$this->$class->enqueue( 'mph-minify-' . $class . '-' . $file );
 
 	}
 
-	function process_styles( $group ) {
-		
-		$styles = array();
+	function get_asset_path( $class, $handle ) {
 
-		foreach ( $this->styles_queue[$group] as $handle ) {
-			
-			$styles[] = $this->get_style_path( $handle );
+		if ( empty( $this->$class->registered[$handle]->src ) )
+			return;
 
-			$this->wp_styles->dequeue( $handle );
+		$src = $this->$class->registered[$handle]->src;
 
-		}	
+		if ( ! preg_match('|^(https?:)?//|', $src) && ! ( $this->$class->content_url && 0 === strpos( $src, $this->$class->content_url ) ) )
+			$src = $this->$class->base_url . $src;
 
-		$min_src = $this->minify_url . '/?f=' . implode( ',', $styles );
-		
-		// Enqueue the scripts
-		$this->wp_styles->add( 'mph-minify-style-group-' . $group, $min_src );
-		$this->wp_styles->add_data( 'mph-minify-style-group-' . $group, 'group', $group );		
-		$this->wp_styles->enqueue( 'mph-minify-style-group-' . $group );
-
-	}
-
-
-	function get_script_path( $handle ) {
-
-		$src = $this->wp_scripts->registered[$handle]->src;
-
-		if ( ! preg_match('|^(https?:)?//|', $src) && ! ( $this->wp_scripts->content_url && 0 === strpos( $src, $this->wp_scripts->content_url ) ) )
-			$src = $this->wp_scripts->base_url . $src;
-
-		return str_replace( home_url(), '', $src );
-
-	}
-
-	function get_style_path( $handle ) {
-
-		$src = $this->wp_styles->registered[$handle]->src;
-
-		if ( ! preg_match('|^(https?:)?//|', $src) && ! ( $this->wp_styles->content_url && 0 === strpos( $src, $this->wp_styles->content_url ) ) )
-			$src = $this->wp_styles->base_url . $src;
+		// Don't handle remote urls. For now...
+		if ( 0 !== strpos( $src, home_url() ) )
+			return;
 
 		return str_replace( home_url(), '', $src );
 
