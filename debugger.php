f6a5fb4c718ab62651a5013b125ddfd3d3d7514e
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-10-04 12:25:09 +0100

    fix some conflicts in the debugger

--- debugger.php
+++ debugger.php
@@ -6,13 +6,13 @@ function mph_minify_debugger_style() {
 	?>
 
 	<style>
-		#mph-minify-debugger { position: fixed; top: 50px; right: 30px; width: 160px; border-radius: 10px; background: rgba(0,0,0,0.8); border: 1px solid rgba(0,0,0,0.5); color: #FFF; padding: 15px 30px; margin-bottom: 30px; }
+		#mph-minify-debugger { position: fixed; top: 50px; right: 30px; width: 160px; border-radius: 10px; background: rgba(0,0,0,0.8); border: 1px solid rgba(0,0,0,0.5); color: #FFF; padding: 15px 30px; margin-bottom: 30px; z-index: 9999; }
 		#mph-minify-debugger h2 { font-family: sans-serif; font-size: 18px; line-height: 1.5; margin-bottom: 5px; letter-spacing: normal; color: #FFF; font-size: 12px; font-family: verdana, sans-serif; }
 		#mph-minify-debugger ul { margin-bottom: 15px; }
 		#mph-minify-debugger ul,
 		#mph-minify-debugger li { list-style: disc outside; padding: 0; margin-left: 0; margin-right: 0; font-size: 10px; font-family: verdana, sans-serif; line-height: 1.5; }
-		#mph-minify-debugger li.header { color: orange;}
-		#mph-minify-debugger li.footer { color: yellow;}
+		#mph-minify-debugger li.mph-min-header { color: orange;}
+		#mph-minify-debugger li.mph-min-footer { color: yellow;}
 
 	</style>
 
@@ -58,9 +58,9 @@ function mph_minify_debugger( $minifiers ) {
 		if ( 0 === strpos( $handle, 'mph-minify' ) )
 			continue;			
 		elseif ( ! empty( $min_script_queue[0] ) && array_key_exists( $handle, $min_script_queue[0] ) )
-			echo '<li class="header">' . $handle . '</li>';
+			echo '<li class="mph-min-header">' . $handle . '</li>';
 		elseif ( ! empty( $min_script_queue[1] ) && array_key_exists( $handle, $min_script_queue[1] ) )
-			echo '<li class="footer">' . $handle . '</li>';
+			echo '<li class="mph-min-footer">' . $handle . '</li>';
 		else
 			echo '<li>' . $handle . '</li>';
 	
@@ -86,15 +86,15 @@ function mph_minify_debugger( $minifiers ) {
 		if ( 0 === strpos( $handle, 'mph-minify' ) )
 			continue;
 		elseif ( isset( $min_style_queue ) && ! empty( $min_style_queue[0] ) && array_key_exists( $handle, $min_style_queue[0] ) )
-			echo '<li class="header">' . $handle . '</li>';
+			echo '<li class="mph-min-header">' . $handle . '</li>';
 		elseif ( isset( $min_style_queue ) && ! empty( $min_style_queue[1] ) && array_key_exists( $handle, $min_style_queue[1] ) )
-			echo '<li class="footer">' . $handle . '</li>';
+			echo '<li class="mph-min-footer">' . $handle . '</li>';
 		else
 			echo '<li>' . $handle . '</li>';
 
 	echo '</ul>';
 	
-	echo '<h2>Key</h2><ul><li class="header">Orange: Minified in header</li><li class="footer">Yellow: Minified in footer</li><li>White: not minified.</li></ul>';
+	echo '<h2>Key</h2><ul><li class="mph-min-header">Orange: Minified in header</li><li class="mph-min-footer">Yellow: Minified in footer</li><li>White: not minified.</li></ul>';
 
 	echo '</div>';
 
