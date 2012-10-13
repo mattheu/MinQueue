718873e04e29efccf6b5c026ae1e5a7dc825f583
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-10-13 20:58:46 +0100

    fix some errors for undefined variables if nothing has been set

--- class.mph-minify-admin.php
+++ class.mph-minify-admin.php
@@ -176,8 +176,7 @@ class MPH_Minify_Admin {
 	 */
 	function field_scripts() {
 
-		$value = ( ! empty( $this->options['scripts_manual'] ) ) ? $this->options['scripts_manual'] : array(); 
-		
+		$values = ( ! empty( $this->options['scripts_manual'] ) ) ? $this->options['scripts_manual'] : array(); 
 
 		?>
 
@@ -186,8 +185,9 @@ class MPH_Minify_Admin {
 				<strong>Minfy & Concatenate Queue</strong> 
 				<span class="description">List of script handles to minify and concatenate into one file. Comma separated or on a new line</span>
 			</label>
-			<textarea id="mph_minify_field_manual_scripts_1" name="mph_minify_options[scripts_manual][]" class="large-text code"><?php echo esc_attr( implode( ',', $value[0] ) ); ?></textarea>
-			<!--<textarea id="mph_minify_field_manual_scripts_2" name="mph_minify_options[scripts_manual][]" class="large-text code"><?php echo esc_attr( implode( ',', $value[1] ) ); ?></textarea>-->
+			<?php foreach( $values as $value ) : ?>
+				<textarea id="mph_minify_field_manual_scripts_1" name="mph_minify_options[scripts_manual][]" class="large-text code"><?php echo ( ! empty( $value ) ) ? esc_attr( implode( ',', $value ) ) : null; ?></textarea>
+			<?php endforeach; ?>
 		</div>
 
 		<div id="field_disabled_scripts">
@@ -244,8 +244,8 @@ class MPH_Minify_Admin {
 
 	function field_styles() {
 
-		$value = ( ! empty( $this->options['styles_manual'] ) ) ? $this->options['styles_manual'] : array(); 
-		
+		$values = ( ! empty( $this->options['styles_manual'] ) ) ? $this->options['styles_manual'] : array(); 
+
 		?>
 
 		<div id="field_manual_styles">
@@ -253,8 +253,9 @@ class MPH_Minify_Admin {
 				<strong>Manual styles</strong> 
 				<span class="description">List of style handles to minify and concatenate into one file. Comma separated or on a new line</span>
 			</label>
-			<textarea id="mph_minify_field_manual_styles" name="mph_minify_options[styles_manual][]" class="large-text code"><?php echo esc_attr( implode( ',', $value[0] ) ); ?></textarea>
-			<!--<textarea id="mph_minify_field_manual_styles" name="mph_minify_options[styles_manual][]" class="large-text code"><?php echo esc_attr( implode( ',', $value[1] ) ); ?></textarea>-->
+			<?php foreach( $values as $value ) : ?>
+				<textarea id="mph_minify_field_manual_styles_1" name="mph_minify_options[styles_manual][]" class="large-text code"><?php echo ( ! empty( $value ) ) ? esc_attr( implode( ',', $value ) ) : null; ?></textarea>
+			<?php endforeach; ?>
 		</div>
 
 		<div id="field_disabled_styles">
