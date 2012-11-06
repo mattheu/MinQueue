f1a3f29b83ee428248ccd14c57472dd03422bb4f
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-11-06 10:34:54 +0000

    bump version number

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@ Plugin Name: MPH Minify
 Plugin URI: http://matth.eu
 Description: Mega Simple Minify. Minifies enqueued scripts & styles.
 Author: Matthew Haines-Young
-Version: 0.1
+Version: 0.2
 Author URI: http://www.matth.eu
 */
 
@@ -13,7 +13,7 @@ require_once( 'class.mph-minify.php' );
 require_once( 'class.mph-minify-admin.php' );
 require_once( 'debugger.php' );
 
-define( 'MPH_MINIFY_VERSION', '0.0.1' );
+define( 'MPH_MINIFY_VERSION', '0.0.2' );
 
 $minified_deps = array( 'WP_Scripts' => array(), 'WP_Styles' => array() );
 global $minified_deps;
@@ -24,14 +24,14 @@ if ( ! defined( 'MPH_MINIFY_OPTIONS' ) )
 add_action( 'wp_enqueue_scripts', 'mph_minify', 9999 );
 add_filter( 'mph_minify_cache_dir', 'mph_minify_cache_dir_override' );
 
-/** 
+/**
  * Return the function options.
  *
  * Handles defaults & overriding with define.
  */
 function mph_minify_get_plugin_options() {
 
-	$defaults = array( 
+	$defaults = array(
 		'debugger' => false,
 		'cache_dir' => 'mph_minify_cache',
 		'scripts_method' => 'disabled',
@@ -51,40 +51,40 @@ function mph_minify_get_plugin_options() {
 
 /**
  * Main Plugin Functionality.
- * 
+ *
  * @return null
  */
 function mph_minify() {
 
 	$options = mph_minify_get_plugin_options();
 
-	$instances = array( 
+	$instances = array(
 		'scripts' => array(),
 		'styles' => array()
 	);
 
-	// Scripts	
-	if ( isset( $options['scripts_method'] ) && 'disabled' !== $options['scripts_method'] ) {	
+	// Scripts
+	if ( isset( $options['scripts_method'] ) && 'disabled' !== $options['scripts_method'] ) {
 
 		foreach ( $options['scripts_manual'] as $key => $queue ) {
 
-			if ( ! empty( $queue ) ) { 
-				$instances['scripts'][$key] = new MPH_Minify( 'WP_Scripts' ); 
+			if ( ! empty( $queue ) ) {
+				$instances['scripts'][$key] = new MPH_Minify( 'WP_Scripts' );
 				$instances['scripts'][$key]->queue = (array) $queue;
 				$instances['scripts'][$key]->minify();
 			}
 
 		}
 
-	}	
+	}
 
 	// Styles
 	if ( isset( $options['styles_method'] ) && 'disabled' !== $options['styles_method'] ) {
 
 		foreach ( $options['styles_manual'] as $key => $queue ) {
 
-			if ( ! empty( $queue ) ) { 
-				$instances['styles'][$key] = new MPH_Minify( 'WP_Styles' ); 
+			if ( ! empty( $queue ) ) {
+				$instances['styles'][$key] = new MPH_Minify( 'WP_Styles' );
 				$instances['styles'][$key]->queue = (array) $queue;
 				$instances['styles'][$key]->minify();
 			}
@@ -109,7 +109,7 @@ function mph_minify() {
  * Filter the cache directory to allow setting your own.
  *
  * If settings or defined
- * 
+ *
  * @param  [type] $cache_dir [description]
  * @return [type]            [description]
  */
